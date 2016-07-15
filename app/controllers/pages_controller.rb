ffaae9372299ea5cc0e06c2c2fbe3f04f1d5b5f1
Kaushik <kaushikm@andrew.cmu.edu>
2016-07-15 15:20:29 +0000

    Adding in requests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
+require 'open-uri'
+require 'openssl'
+require 'json'
+
 class PagesController < ApplicationController
   
   def home
@@ -9,6 +13,11 @@ class PagesController < ApplicationController
   end
   
   def content
+    
+    OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
+    url = 'https://api.fda.gov/drug/event.json?api_key=hQ4jRHCt6WIYjcDYHBvIxIbMNPhlg5Hyw9WLhpLQ&search=_missing_:companynumb&limit=1'
+    response = open(url).read
+    @result = response.split("\n")
     
   end
   
