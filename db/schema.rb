f06e03d568d48585d1887e3403d1cd55643df3ab
Kaushik <kaushikm@andrew.cmu.edu>
2016-07-20 13:12:41 +0000

    rollback

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160717035530) do
-
-  create_table "tables", force: :cascade do |t|
-    t.string "time"
-    t.string "count"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
