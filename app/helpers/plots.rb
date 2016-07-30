dcb0ed728e0de54b92220fcfc32801e10bad7adf
Kaushik <kaushikm@andrew.cmu.edu>
2016-07-30 14:22:02 +0000

    Edited plots

--- app/helpers/plots.rb
+++ app/helpers/plots.rb
@@ -32,7 +32,7 @@ module Plots
     OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
     year_list = ['[1990-01-01+TO+2000-01-01]','[2000-01-01+TO+2010-01-01]']
     for index in 0..1
-      url = 'https://api.fda.gov/device/510k.json?limit=100&search=date_received:' + year_list[index] 
+      url = 'https://api.fda.gov/device/510k.json?limit=100&search=medical_specialty_description:"Ophthalmic"+AND+date_received:' + year_list[index] 
       total = findingTotal(url)
       if index == 0
         my_dict['1990-1999'] = total
@@ -48,7 +48,7 @@ module Plots
     OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
     for value in 1..3
       #url = 'https://api.fda.gov/device/510k.json?api_key=hQ4jRHCt6WIYjcDYHBvIxIbMNPhlg5Hyw9WLhpLQlimit=100&search=date_received:[1990-01-01+TO+2010-01-01]+AND+device_class:' + value.to_s
-      url = 'https://api.fda.gov/device/510k.json?limit=100&search=date_received:[1990-01-01+TO+2010-01-01]+AND+device_class:' + value.to_s
+      url = 'https://api.fda.gov/device/510k.json?limit=100&search=medical_specialty_description:"Ophthalmic"+AND+date_received:[1990-01-01+TO+2010-01-01]+AND+device_class:' + value.to_s
       if value == 1
         my_dict['1'] = findingTotal(url)
       elsif value == 2
@@ -58,6 +58,11 @@ module Plots
       end
     end
     return my_dict
+  end
+  
+  def my_helperFour
+    
+    url = 'https://api.fda.gov/device/510k.json?limit=100&search=state:"ca"'
     
   end
   
