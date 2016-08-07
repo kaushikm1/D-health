8a9207cada38053a2cd3e782f910017aa33d805a
Kaushik <kaushikm@andrew.cmu.edu>
2016-08-07 06:30:38 +0000

    Formatting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,6 +26,7 @@ include Plots
     @plotTwo = my_helperTwo
     @yearly2000s = yearly_plot(0,15)
     @yearly90s = yearly_plot(90,99)
+    @totalyears = @yearly90s.merge(@yearly2000s)
     
     
   end
@@ -35,6 +36,7 @@ include Plots
     @plotThree = my_helperThree
     @classTwoyearly90s = classtwo_yearly_plot(90,99)
     @classTwoyearly00s = classtwo_yearly_plot(0,15)
+    @classTwototalyears = @classTwoyearly90s.merge(@classTwoyearly00s)
     
   end
   
